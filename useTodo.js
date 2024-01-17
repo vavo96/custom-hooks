5783942d8424255762f026fd81cddfd20f2aa9fb
osmar_vasquez <osmar.vasquez@dacodes.com.mx>
2024-01-17 11:30:38 -0600

    mi primer commit

---
+++ useTodo.js
@@ -0,0 +1,49 @@
+import { useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const initialState = [
+    //{
+    //    id: new Date().getTime(),
+    //    description: 'Recolectar la piedra del alma',
+    //    done: false
+    //},
+]
+
+const init = () => {
+    return JSON.parse( localStorage.getItem('todos') ) || [];
+} 
+
+export const useTodo = ( ) => {
+
+    const [ todos, dispatch ] = useReducer( todoReducer, initialState, init );
+    
+    const handleNewTodo = (todo) =>{
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo,
+        }
+
+        dispatch(action);
+    }
+
+    const handleDeleteTodo = (id) => {
+        dispatch({
+            type: '[TODO] Remove Todo',
+            payload: id
+        });
+    }
+
+    const handleToggleTodo = (id) => {
+        dispatch({
+            type:'[TODO] Toggle Todo',
+            payload: id
+        })
+    }
+
+    return {
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo,
+        todos
+    }
+}
